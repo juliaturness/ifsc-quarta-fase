99c3d29c0e791d0b006e486a8e55b1138da2f1fe
juliaturness <jmturnes10@gmail.com>
2025-08-28 17:04:42 -0300

    iniciando

---
+++ pbe/agenda/src/main/java/ifsc/julia/IfscApplication.java
@@ -0,0 +1,13 @@
+package ifsc.julia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfscApplication.class, args);
+	}
+
+}
---
+++ pbe/agenda/src/test/java/ifsc/julia/IfscApplicationTests.java
@@ -0,0 +1,13 @@
+package ifsc.julia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
